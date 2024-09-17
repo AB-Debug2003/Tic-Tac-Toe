9f5ab32eeb34bfbce7b9c0e209025e609132b57e
Abdullah <abdullahshaheen311@gmail.com>
2024-09-17 15:14:51 +0500

    Vercel speed insights configured

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
