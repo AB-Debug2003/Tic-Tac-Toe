41068e7083153b8f5d5b4e96540f6609678d794c
Abdullah <abdullahshaheen311@gmail.com>
2024-09-17 15:16:42 +0500

    adjusted speed insights code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Tictac from './Tictac'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
 
   return (
     <>
       <Tictac/>
+      <SpeedInsights />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
 )
